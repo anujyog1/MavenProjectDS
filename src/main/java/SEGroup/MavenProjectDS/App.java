cd6f17e7aa18cf206aa2f71103649ccad00c8cc3
yogita <agyogita@gmail.com>
2023-10-26 15:42:28 +0530

    committing maven java project

---
+++ src/main/java/SEGroup/MavenProjectDS/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenProjectDS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Data Science students" );
+    }
+}
